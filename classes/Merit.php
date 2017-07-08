1c7761b4c1a88398df47c67192eac44d2d5f7b1b
Jack-Barradell <jirbarradell-johns1@sheffield.ac.uk>
2017-07-08 17:32:14 +0100

    merit class stub

---
+++ classes/Merit.php
@@ -0,0 +1,17 @@
+<?php
+namespace AMC\Classes;
+
+class Merit implements DataObject {
+    use Getable;
+    use Storable;
+
+    private $_id = null;
+    private $_userID = null;
+    private $_adminID = null;
+    private $_meritValue = null;
+    private $_mertiReason = null;
+    private $_meritTimestamp = null;
+
+    // TODO: Implement
+
+}
