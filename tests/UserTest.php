d2f96247853c196f3f929d349ba82b88e602368c
Jack Barradell <jirbarradell-johns1@sheffield.ac.uk>
2017-08-03 02:39:07 +0100

    some user unit tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -12,6 +12,7 @@ require '../classes/exceptions/BlankObjectException.php';
 use AMC\Classes\Faction;
 use AMC\Classes\User;
 use AMC\Classes\Database;
+use AMC\Exceptions\BlankObjectException;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase {
@@ -72,18 +73,159 @@ class UserTest extends TestCase {
         $testUser->setFactionID($testFaction->getID());
         $testUser->create();
 
+        // Check id is an int
+        $this->assertInternalType('int', $testUser->getID());
+
+        // Now pull and check
+        $stmt = $this->_connection->prepare("SELECT `UserID`,`Username`,`PasswordHash`,`Email`,`Banned`,`Activated`,`LastLogin`,`SystemAccount`,`FactionID` FROM `Users` WHERE `UserID`=?");
+        $stmt->bind_param('i', $testUser->getID());
+        $stmt->execute();
+        $stmt->bind_result($userID, $username, $passwordHash, $email, $banned, $activated, $lastLogin, $systemAccount, $factionID);
+
+        // Check there is one result
+        $this->assertEquals(1, $stmt->num_rows);
+
+        $stmt->fetch();
+
+        if($banned == 1) {
+            $banned = true;
+        }
+        else {
+            $banned = false;
+        }
+        if($activated == 1) {
+            $activated = true;
+        }
+        else {
+            $activated = false;
+        }
+        if($systemAccount == 1) {
+            $systemAccount = true;
+        }
+        else {
+            $systemAccount = false;
+        }
+
+        $this->assertEquals($testUser->getID(), $userID);
+        $this->assertEquals($testUser->getUsername(), $username);
+        $this->assertEquals($testUser->getPasswordHash(), $passwordHash);
+        $this->assertEquals($testUser->getEmail(), $email);
+        $this->assertEquals($testUser->getBanned(), $banned);
+        $this->assertEquals($testUser->getActivated(), $activated);
+        $this->assertEquals($testUser->getLastLogin(), $lastLogin);
+        $this->assertEquals($testUser->getSystemAccount(), $systemAccount);
+        $this->assertEquals($testUser->getFactionID(), $factionID);
+
+        $stmt->close();
+
+        // Clean up
+        $testUser->delete();
+        $testFaction->delete();
     }
 
     public function testBlankCreate() {
+        // Create a test user
+        $user = new User();
 
+        // Set expected exception
+        $this->expectException(BlankObjectException::class);
+
+        // Trigger it
+        $user->create();
     }
 
     public function testUpdate() {
+        // Create a test faction
+        $testFaction = [];
+        $testFaction[0] = new Faction();
+        $testFaction[0]->setName('test');
+        $testFaction[0]->create();
 
+        $testFaction[1] = new Faction();
+        $testFaction[1]->setName('test');
+        $testFaction[1]->create();
+
+        // Create a test user
+        $testUser = new User();
+        $testUser->setUsername('username');
+        $testUser->setPasswordHash('hashed');
+        $testUser->setEmail('test@email.com');
+        $testUser->setBanned(false);
+        $testUser->setActivated(true);
+        $testUser->setLastLogin(123);
+        $testUser->setSystemAccount(false);
+        $testUser->setFactionID($testFaction[0]->getID());
+        $testUser->create();
+
+        // Now update it
+        $testUser->setUsername('username2');
+        $testUser->setPasswordHash('hashed2');
+        $testUser->setEmail('test@email2.com');
+        $testUser->setBanned(true);
+        $testUser->setActivated(false);
+        $testUser->setLastLogin(12345);
+        $testUser->setSystemAccount(true);
+        $testUser->setFactionID($testFaction[1]->getID());
+        $testUser->update();
+
+        // Now pull and check
+        $stmt = $this->_connection->prepare("SELECT `UserID`,`Username`,`PasswordHash`,`Email`,`Banned`,`Activated`,`LastLogin`,`SystemAccount`,`FactionID` FROM `Users` WHERE `UserID`=?");
+        $stmt->bind_param('i', $testUser->getID());
+        $stmt->execute();
+        $stmt->bind_result($userID, $username, $passwordHash, $email, $banned, $activated, $lastLogin, $systemAccount, $factionID);
+
+        // Check there is one result
+        $this->assertEquals(1, $stmt->num_rows);
+
+        $stmt->fetch();
+
+        if($banned == 1) {
+            $banned = true;
+        }
+        else {
+            $banned = false;
+        }
+        if($activated == 1) {
+            $activated = true;
+        }
+        else {
+            $activated = false;
+        }
+        if($systemAccount == 1) {
+            $systemAccount = true;
+        }
+        else {
+            $systemAccount = false;
+        }
+
+        $this->assertEquals($testUser->getID(), $userID);
+        $this->assertEquals($testUser->getUsername(), $username);
+        $this->assertEquals($testUser->getPasswordHash(), $passwordHash);
+        $this->assertEquals($testUser->getEmail(), $email);
+        $this->assertEquals($testUser->getBanned(), $banned);
+        $this->assertEquals($testUser->getActivated(), $activated);
+        $this->assertEquals($testUser->getLastLogin(), $lastLogin);
+        $this->assertEquals($testUser->getSystemAccount(), $systemAccount);
+        $this->assertEquals($testUser->getFactionID(), $factionID);
+
+        $stmt->close();
+
+        // Clean up
+        $testUser->delete();
+        foreach($testFaction as $faction) {
+            $faction->delete();
+        }
     }
 
     public function testBlankUpdate() {
+        // Create a test user
+        $user = new User();
 
+        // Set expected exception
+        $this->expectException(BlankObjectException::class);
+
+        // Trigger it
+        $user->update();
     }
 
     public function testDelete() {
