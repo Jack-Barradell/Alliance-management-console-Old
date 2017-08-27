6f80c73fbc14a8479c30b41da8825c89e129454a
Jack Barradell <jirbarradell-johns1@sheffield.ac.uk>
2017-08-27 02:29:35 +0100

    intelligence type tests

--- tests/IntelligenceTypeTest.php
+++ tests/IntelligenceTypeTest.php
@@ -11,6 +11,7 @@ require '../classes/exceptions/BlankObjectException.php';
 
 use AMC\Classes\IntelligenceType;
 use AMC\Classes\Database;
+use AMC\Exceptions\BlankObjectException;
 use PHPUnit\Framework\TestCase;
 
 class IntelligenceTypeTest extends TestCase {
@@ -38,23 +39,118 @@ class IntelligenceTypeTest extends TestCase {
     }
 
     public function testCreate() {
-        //TODO: Implement
+        //Create a test intelligence type
+        $testIntelligenceType = new IntelligenceType();
+        $testIntelligenceType->setName('testName');
+        $testIntelligenceType->create();
+
+        // Check id is now an int
+        $this->assertInternalType('int', $testIntelligenceType->getID());
+
+        // Pull and check
+        $stmt = $this->_connection->prepare("SELECT `IntelligenceTypeID`,`IntelligenceTypeName` FROM `Intelligence_Types` WHERE `IntelligenceTypeID`=?");
+        $stmt->bind_param('i', $testIntelligenceType->getID());
+        $stmt->execute();
+        $stmt->bind_result($intelligenceTypeID, $name);
+
+        // Check there is only one result
+        $this->assertEquals(1, $stmt->num_rows);
+
+        $stmt->fetch();
+
+        $this->assertEquals($testIntelligenceType->getID(), $intelligenceTypeID);
+        $this->assertEquals($testIntelligenceType->getName(), $name);
+
+        $stmt->close();
+
+        // Clean up
+        $testIntelligenceType->delete();
     }
 
     public function testBlankCreate() {
-        //TODO: Implement
+        // Create a test intelligence type
+        $testIntelligenceType = new IntelligenceType();
+
+        // Set expected exception
+        $this->expectException(BlankObjectException::class);
+
+        // Trigger it
+        try {
+            $testIntelligenceType->create();
+        } catch(BlankObjectException $e) {
+            $this->assertEquals('Cannot store a blank Intelligence Type.', $e->getMessage());
+        }
     }
 
     public function testUpdate() {
-        //TODO: Implement
+        //Create a test intelligence type
+        $testIntelligenceType = new IntelligenceType();
+        $testIntelligenceType->setName('testName');
+        $testIntelligenceType->create();
+
+        // Now update it
+        $testIntelligenceType->setName('testName2');
+        $testIntelligenceType->update();
+
+        // Pull and check
+        $stmt = $this->_connection->prepare("SELECT `IntelligenceTypeID`,`IntelligenceTypeName` FROM `Intelligence_Types` WHERE `IntelligenceTypeID`=?");
+        $stmt->bind_param('i', $testIntelligenceType->getID());
+        $stmt->execute();
+        $stmt->bind_result($intelligenceTypeID, $name);
+
+        // Check there is only one result
+        $this->assertEquals(1, $stmt->num_rows);
+
+        $stmt->fetch();
+
+        $this->assertEquals($testIntelligenceType->getID(), $intelligenceTypeID);
+        $this->assertEquals($testIntelligenceType->getName(), $name);
+
+        $stmt->close();
+
+        // Clean up
+        $testIntelligenceType->delete();
     }
 
     public function testBlankUpdate() {
-        //TODO: Implement
+        // Create a test intelligence type
+        $testIntelligenceType = new IntelligenceType();
+
+        // Set expected exception
+        $this->expectException(BlankObjectException::class);
+
+        // Trigger it
+        try {
+            $testIntelligenceType->update();
+        } catch(BlankObjectException $e) {
+            $this->assertEquals('Cannot store a blank Intelligence Type.', $e->getMessage());
+        }
     }
 
     public function testDelete() {
-        //TODO: Implement
+        //Create a test intelligence type
+        $testIntelligenceType = new IntelligenceType();
+        $testIntelligenceType->setName('testName');
+        $testIntelligenceType->create();
+
+        // Save the id
+        $id = $testIntelligenceType->getID();
+
+        // Now delete it
+        $testIntelligenceType->delete();
+
+        // Check id is null
+        $this->assertNull($testIntelligenceType->getID());
+
+        // Pull and check
+        $stmt = $this->_connection->prepare("SELECT `IntelligenceTypeID`,`IntelligenceTypeName` FROM `Intelligence_Types` WHERE `IntelligenceTypeID`=?");
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        // Check there are no results
+        $this->assertEquals(0, $stmt->num_rows);
+
+        $stmt->close();
     }
 
     public function testSelectWithInput() {
